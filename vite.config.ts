384d8883381fdbae94d691c312cb5c8163b27264
DESKTOP-QDVVNUR\Loc <ltchuong19@gmail.com>
2023-09-22 22:12:04 -0700

    Setup Vite Configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import generouted from "@generouted/react-router/plugin";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import path from "path";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [svgr(), react(), generouted(), eslint(), tsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
