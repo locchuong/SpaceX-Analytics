2e7aa9f6fe0bd52fccd3f67225159bdfbacd84ed
DESKTOP-QDVVNUR\Loc <ltchuong19@gmail.com>
2023-09-24 00:43:35 -0700

    Label component

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -5,7 +5,18 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "~/lib/tailwind";
 
-const labelVariants = cva("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70");
+const labelVariants = cva(
+  cn(
+    // Unfocused Filled state
+    "absolute left-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm leading-none text-zinc-800 duration-300",
+    // Empty state
+    "peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-zinc-500",
+    // On focus
+    "peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-zinc-800",
+    // Disabled
+    "peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+  ),
+);
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
