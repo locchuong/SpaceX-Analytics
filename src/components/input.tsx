a6d8a6820a8e61aabb73482bdddc9228492d8b16
DESKTOP-QDVVNUR\Loc <ltchuong19@gmail.com>
2023-09-24 00:43:14 -0700

    Input Component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,20 +1,26 @@
 import * as React from "react";
 
+import { cva, VariantProps } from "class-variance-authority";
+
 import { cn } from "~/lib/tailwind";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => (
-  <input
-    type={type}
-    className={cn(
-      "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-      className,
-    )}
-    ref={ref}
-    {...props}
-  />
-));
+const inputVariants = cva(
+  cn(
+    "peer relative flex h-12 w-full border border-neutral-200 bg-transparent px-[10px] py-1 pb-[10px] pt-4 text-sm transition-colors dark:border-white",
+    "placeholder:text-muted-foreground",
+    "disabled:cursor-not-allowed disabled:opacity-50",
+    "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-300 dark:focus-visible:ring-blue-400",
+    "file:border-0 file:bg-transparent file:text-sm file:font-medium",
+  ),
+);
+
+const Input = React.forwardRef<HTMLInputElement, InputProps & VariantProps<typeof inputVariants>>(
+  ({ className, type, placeholder, ...props }, ref) => (
+    <input type={type} className={cn(inputVariants(), className)} ref={ref} placeholder={placeholder ?? " "} {...props} />
+  ),
+);
 Input.displayName = "Input";
 
 export { Input };
