6745b3c8a10c8e142f3471da4a0f55cb72967d5a
DESKTOP-QDVVNUR\Loc <ltchuong19@gmail.com>
2023-09-28 10:23:04 -0700

    Mock api in prod

--- src/features/astronaut/api/getAstronauts.ts
+++ src/features/astronaut/api/getAstronauts.ts
@@ -1,6 +1,9 @@
 import { QueryFunction, QueryKey, useQuery, UseQueryOptions, UseQueryResult } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 
 import { axios, ErrorType } from "~/lib/axios";
+
+import MOCK_DATA from "../constants/astronaut-mock-api-data.json";
 
 const apiRoute = "/astros.json";
 
@@ -11,11 +14,19 @@ type GetAstronautsAPIResponse = {
   people: { name: string; craft: string }[];
 };
 
+// ? API has to be mocked in production.
+// ? API is HTTP, Domain is HTTPS
+const MOCKED_API: AxiosResponse<GetAstronautsAPIResponse> | undefined = {
+  data: MOCK_DATA,
+  status: 200,
+} as AxiosResponse<GetAstronautsAPIResponse> | undefined;
+
 // Query Key
 export const getAstronautsQueryKey = () => [apiRoute];
 
 // Axios Fn
-export const getAstronauts = (signal?: AbortSignal) => axios.get<GetAstronautsAPIResponse>(apiRoute, { signal });
+export const getAstronauts = (signal?: AbortSignal) =>
+  import.meta.env.PROD ? MOCKED_API : axios.get<GetAstronautsAPIResponse>(apiRoute, { signal });
 
 // useQuery Hook
 export const useGetAstronauts = <TData = Awaited<ReturnType<typeof getAstronauts>>, TError = ErrorType<Error>>(options?: {
