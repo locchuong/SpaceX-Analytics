e6e044bf73c9de8887fbe5904bca74b5992d3e4a
DESKTOP-QDVVNUR\Loc <ltchuong19@gmail.com>
2023-09-28 10:26:48 -0700

    Mock ISS Now API on Prod

--- src/features/space-station/api/getISSLocation.ts
+++ src/features/space-station/api/getISSLocation.ts
@@ -1,6 +1,9 @@
 import { QueryFunction, QueryKey, useQuery, UseQueryOptions, UseQueryResult } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 
 import { axios, ErrorType } from "~/lib/axios";
+
+import MOCK_DATA from "../constants/space-station-mock-api-data.json";
 
 const apiRoute = "/iss-now.json";
 
@@ -14,11 +17,19 @@ type GetISSLocationAPIResponse = {
   };
 };
 
+// ? API has to be mocked in production.
+// ? API is HTTP, Domain is HTTPS
+const MOCKED_API: AxiosResponse<GetISSLocationAPIResponse> | undefined = {
+  data: MOCK_DATA,
+  status: 200,
+} as AxiosResponse<GetISSLocationAPIResponse> | undefined;
+
 // Query Key
 export const getISSLocationQueryKey = () => [apiRoute];
 
 // Axios Fn
-export const getISSLocation = (signal?: AbortSignal) => axios.get<GetISSLocationAPIResponse>(apiRoute, { signal });
+export const getISSLocation = (signal?: AbortSignal) =>
+  import.meta.env.PROD ? MOCKED_API : axios.get<GetISSLocationAPIResponse>(apiRoute, { signal });
 
 // useQuery Hook
 export const useGetISSLocation = <TData = Awaited<ReturnType<typeof getISSLocation>>, TError = ErrorType<Error>>(options?: {
